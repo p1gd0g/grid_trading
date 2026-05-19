a164e3aa1a394d93583341cc6ffa1f80baf8ea4e
p1gd0g <renhao.szu@gmail.com>
2026-05-19 18:48:42 +0800

    更新版本号至 0.0.10，添加 `TradeType` 属性并实现价格触发逻辑

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.10
+
+init `Transaction`.
+
 ## 0.0.9
 
 init `Transaction`.
--- lib/data/transaction.dart
+++ lib/data/transaction.dart
@@ -1,7 +1,23 @@
 import 'package:grid_trading/enum/price_range_type.dart';
+import 'package:grid_trading/enum/trade_type.dart';
 
 class Transaction {
   final PriceRangeType priceRangeType;
+  final TradeType tradeType;
 
-  Transaction({required this.priceRangeType});
+  Transaction({required this.priceRangeType, required this.tradeType});
+
+  double nextTriggerSellPrice(double tmpPrice, double upPrice) {
+    if (priceRangeType == PriceRangeType.percent) {
+      return tmpPrice * (1 + upPrice);
+    }
+    return tmpPrice + upPrice;
+  }
+
+  double nextTriggerBuyPrice(double tmpPrice, double upPrice) {
+    if (priceRangeType == PriceRangeType.percent) {
+      return tmpPrice * (1 - upPrice);
+    }
+    return tmpPrice - upPrice;
+  }
 }
