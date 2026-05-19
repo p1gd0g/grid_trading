579d5371c2230bae46d04f6f80a3c9228fe75fef
p1gd0g <renhao.szu@gmail.com>
2026-05-19 18:32:20 +0800

    更新版本号至 0.0.9，初始化 `Transaction` 类

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.9
+
+init `Transaction`.
+
 ## 0.0.8
 
 add enums.
---
+++ lib/data/transaction.dart
@@ -0,0 +1,7 @@
+import 'package:grid_trading/enum/price_range_type.dart';
+
+class Transaction {
+  final PriceRangeType priceRangeType;
+
+  Transaction({required this.priceRangeType});
+}
